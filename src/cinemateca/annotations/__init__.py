bc5fb4d9146446997aceee37163d578a171f2c9a
guto-mojica <mojica@duck.com>
2026-05-25 09:41:08 -0400

    fix(annotations): re-export FILENAME from package __init__ (T6 prep)

--- src/cinemateca/annotations/__init__.py
+++ src/cinemateca/annotations/__init__.py
@@ -2,12 +2,14 @@
 from __future__ import annotations
 
 from cinemateca.annotations.io import (
+    FILENAME,
     load,
     merge_tag_index,
     save,
 )
 
 __all__ = [
+    "FILENAME",
     "load",
     "merge_tag_index",
     "save",
